40c1a8c7de15af9d4f43758a7e6e8eb09ee9cd92
Vasu <vasu.namdevcs2021@indoreinstitute.com>
2024-03-23 13:59:25 +0530

    update

--- src/main/java/com/spring/chatroomapp/config/Config.java
+++ src/main/java/com/spring/chatroomapp/config/Config.java
@@ -2,10 +2,14 @@ package com.spring.chatroomapp.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -19,5 +23,10 @@ public class Config implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        return WebSocketMessageBrokerConfigurer.super.configureMessageConverters(messageConverters);
     }
 }
